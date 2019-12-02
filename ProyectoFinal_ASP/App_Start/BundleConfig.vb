315cd9e4ce359d1a1458092e05130925ef97fdb9
unknown <you@example.com>
2019-12-02 12:19:47 -0500

    first commit

---
+++ ProyectoFinal_ASP/App_Start/BundleConfig.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Web.Optimization
+
+Public Module BundleConfig
+    ' Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+    Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"))
+
+        bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.validate*"))
+
+        ' Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+        ' para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"))
+
+        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
+                  "~/Scripts/bootstrap.js"))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include(
+                  "~/Content/bootstrap.css",
+                  "~/Content/site.css"))
+    End Sub
+End Module
+
---
+++ ProyectoFinal_ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="ProyectoFinal_ASP.MvcApplication" Language="VB" %>
